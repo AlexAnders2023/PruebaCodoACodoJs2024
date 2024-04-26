9334e49f3cd27e0c267eb77479dba5871866209c
AlexAnders2023 <m.alejandro.q@gmail.com>
2024-04-25 21:45:11 -0300

    primer commit en git de ejemplo1

---
+++ script24148.js
@@ -0,0 +1,44 @@
+console.log("Hola a todos");
+//esto es un comentario
+console.log("La suma de 3+8 es: ", 3 + 8);
+document.write("<h2 style=´color: red;´> Texto incorporado por js");
+console.info("Hola a todos esta es una informacion");//esta linea muetsra informacion
+
+/*comentario linea 1
+ahora vemos varios ejemplos de console
+comentario varias lineas */
+
+console.warn("Hola a todos esta es una adverencia");
+console.error("Hola a todos esto es un error");  
+
+//cambio de valor de una variable con js
+/*
+var n = 10;
+
+document.write("<p>Hola la variable es: </p>",n);
+
+var num = 20;
+
+document.write("<p>Hola la variable es: </p>",num);
+
+var nu = 18;
+
+document.write("<p>Hola la variable es: </p>",nu);
+
+var numero = 50;
+
+document.write("<p>Hola la variable es: </p>",numero);
+*/
+
+//constantes:
+/*
+const PI= 3.141592; 
+const IVA= 21; //la constante no cambia nunca
+
+console.log(PI);
+
+const IVA= 21; 
+IVA= 10.5; 
+console.log(IVA);
+*/
+
